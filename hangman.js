ea1887a824e31aa39615ff02b3e77fa4a4b9d87b
selmanbey <selmannur@yahoo.com>
2017-12-19 07:40:39 +0000

    media queries

--- hangman.js
+++ hangman.js
@@ -3,21 +3,108 @@ $('document').ready( function() {
 
   // Words List
 
-  var hangmanWords = ['adult', 'advice', 'arrangement', 'attempt', 'autumn',
-  'border', 'breeze', 'brick', 'calm', 'canal', 'cast', 'chosen', 'claw', 'coach',
-  'constant', 'contrast', 'cookie', 'custom', 'damage', 'deep', 'depth',
-  'discussion', 'doll', 'donkey', 'essential', 'exchange', 'exist',
-  'explanation', 'face', 'film', 'finest', 'fireplace', 'folk', 'fort',
-  'garage', 'grandmother', 'habit', 'happy', 'hunter', 'image', 'independent',
-  'instant', 'kids', 'label', 'mathematics', 'memory', 'mill', 'mission',
-  'monkey', 'mountain', 'mysterious', 'neighborhood', 'nuts', 'occasional',
-  'official', 'palace', 'plate', 'poetry', 'policeman', 'positive', 'possible',
-  'practical', 'pride', 'promise', 'recall', 'relationship', 'remarkable',
-  'require', 'rhyme', 'rocky', 'rush', 'sale', 'satellite', 'satisfaction',
-  'scary', 'selection', 'shake', 'shallow', 'shout', 'silly', 'simple',
-  'slight', 'slip', 'slope', 'soap', 'solar', 'species', 'spin', 'stiff',
-  'swung', 'tale', 'thumb', 'tobacco', 'toy', 'tray', 'tune', 'university',
-  'vapor', 'vessels', 'wealth', 'wolf', 'zoo'];
+  var hangmanWords = {
+      "accord": "concurrence of opinion",
+      "affect": "have an influence upon",
+      "apparent": "clearly revealed to the mind or the senses or judgment",
+      "appeal": "be attractive to",
+      "appoint": "assign a duty, responsibility or obligation to",
+      "approach": "move towards",
+      "assert": "declare or affirm solemnly and formally as true",
+      "attitude": "a complex mental state involving beliefs and feelings",
+      "attribute": "a quality belonging to or characteristic of an entity",
+      "bent": "fixed in your purpose",
+      "campaign": "a race between candidates for elective office",
+      "chamber": "a natural or artificial enclosed space",
+      "circumstances": "one's overall condition in life",
+      "coast": "the shore of a sea or ocean",
+      "commission": "a special group delegated to consider some matter",
+      "commit": "perform an act, usually with a negative connotation",
+      "compel": "force somebody to do something",
+      "concept": "an abstract or general idea inferred from specific instances",
+      "concern": "something that interests you because it is important",
+      "conduct": "direct the course of, manage or control",
+      "confer": "have a meeting in order to talk something over",
+      "consider": "deem to be",
+      "constant": "a quantity that does not vary",
+      "constitute":"to compose or represent",
+      "contempt": "lack of respect accompanied by a feeling of intense dislike",
+      "contract": "a binding agreement that is enforceable by law",
+      "convention": "a large formal assembly",
+      "convince": "make realize the truth or validity of something",
+      "court": "an assembly to conduct judicial business",
+      "crew": "the men and women who man a vehicle",
+      "despair": "a state in which all hope is lost or absent",
+      "distinction": "a discrimination between things as different",
+      "dwell": "think moodily or anxiously about something",
+      "earnest": "characterized by a firm, humorless belief in one's opinions",
+      "engage": "consume all of one's attention or time",
+      "entertain": "provide amusement for",
+      "establish": "set up or found",
+      "evident": "clearly revealed to the mind or the senses or judgment",
+      "exert": "make a great effort at a mental or physical task",
+      "fancy": "imagine, conceive of, see in one's mind",
+      "financial": "involving fiscal matters",
+      "flag": "a rectangular piece of cloth of distinctive design",
+      "formal": "characteristic of or befitting a person in authority",
+      "furnish": "provide with objects or articles that make a room usable",
+      "generate": "bring into existence",
+      "grant": "allow to have",
+      "harry": "annoy continually or chronically",
+      "humble": "marked by meekness or modesty, not arrogant or prideful",
+      "inclined": "at an angle to the horizontal or vertical position",
+      "insist": "be emphatic or resolute and refuse to budge",
+      "inspire": "serve as the inciting cause of",
+      "instance": "an occurrence of something",
+      "institute": "set up or lay the groundwork for",
+      "intend": "have in mind as a purpose",
+      "intimate": "marked by close acquaintance, association, or familiarity",
+      "issue": "some situation or event that is thought about",
+      "justify": "show to be reasonable or provide adequate ground for",
+      "keen": "demonstrating ability to recognize or draw fine distinctions",
+      "knight": "a person of noble birth trained to arms and chivalry",
+      "labor": "any piece of work that is undertaken or attempted",
+      "league": "an association of sports teams that organizes matches",
+      "level": "a relative position or degree of value in a graded group",
+      "liberal": "having political views favoring reform and progress",
+      "majority": "more than half of the votes in an election",
+      "manifest": "reveal its presence or make an appearance",
+      "merit": "any admirable or beneficial attribute",
+      "minute": "infinitely or immeasurably small",
+      "mode": "how something is done or how it happens",
+      "notion": "a general inclusive concept",
+      "novel": "an extended fictional work in prose",
+      "obtain": "come into possession of",
+      "passage": "a section of text, particularly a section of medium length",
+      "persist": "continue to exist",
+      "plead": "enter a defendant's answer",
+      "policy": "a plan of action adopted by an individual or social group",
+      "practice": "a customary way of operation or behavior",
+      "project": "a planned undertaking",
+      "property": "a basic or essential attribute shared by members of a class",
+      "range": "a variety of different things or activities",
+      "reflect": "show an image of",
+      "render": "give an interpretation of",
+      "resource": "a new or reserve supply that can be drawn upon when needed",
+      "scale": "relative magnitude",
+      "scarce": "deficient in quantity or number compared with the demand",
+      "scheme": "an elaborate and systematic plan of action",
+      "skill": "an ability that has been acquired by training",
+      "stock": "capital raised by a corporation through the issue of shares",
+      "straight": "successive, without a break",
+      "temper": "a characteristic state of feeling",
+      "territory": "the geographical area under the jurisdiction of a state",
+      "theory": "a well-substantiated explanation of some aspect of the world",
+      "tide": "the periodic rise and fall of the sea level",
+      "tour": "a route all the way around a particular place or area",
+      "undertake": "enter upon an activity or enterprise",
+      "utter": "without qualification",
+      "vain": "unproductive of success",
+      "venture": "proceed somewhere despite the risk of possible dangers",
+      "wander": "move or cause to move in a sinuous or circular course",
+      "weigh": "be oppressive or burdensome",
+      "yield": "give or supply",
+  };
 
 
   // JQuery Element Selector Variables
@@ -27,6 +114,7 @@ $('document').ready( function() {
   var notificationBoard = $("#notification")
   var scoreBoard = $(".score_board")
   var bestScoreBoard = $(".best_score_board")
+  var tipBoard = $("#tip")
 
 
   // Game Variables
@@ -34,10 +122,13 @@ $('document').ready( function() {
   var chances = 5;
   var score = 0;
   var bestScore = localStorage.getItem("bestScore")
-  var randomWord = hangmanWords[Math.round(Math.random() * (hangmanWords.length))].toUpperCase();
+  var randomWord = Object.keys(hangmanWords)[Math.round(Math.random() * Object.keys(hangmanWords).length)].toUpperCase();
   var chosenWord = randomWord.split("")
   var guessedWord = [];
   var repeatingLetters = [];
+  console.log(randomWord)
+  var tip = "\"".concat(hangmanWords[randomWord.toLowerCase()]).concat("\"");
+  console.log(tip)
 
   // Pre-Game Functions
 
@@ -66,6 +157,7 @@ $('document').ready( function() {
 
   createBoard(randomWord, guessedWord);
   wordBoard.html(printBoard(guessedWord));
+  tipBoard.html(tip);
   chancesBoard.html(chances)
   colorChances("#0B5345");
   scoreBoard.html(score)
@@ -166,12 +258,14 @@ $('document').ready( function() {
     chancesBoard.html(chances);
     colorChances("#0B5345");
     scoreBoard.html(score)
-    randomWord = hangmanWords[Math.round(Math.random() * (hangmanWords.length))].toUpperCase();
-    chosenWord = randomWord.split("")
+    randomWord = Object.keys(hangmanWords)[Math.round(Math.random() * Object.keys(hangmanWords).length)].toUpperCase();
+    tip = "\"".concat(hangmanWords[randomWord.toLowerCase()]).concat("\"");
+    chosenWord = randomWord.split("");
     guessedWord = [];
     repeatingLetters = [];
     createBoard(randomWord, guessedWord);
     wordBoard.html(printBoard(guessedWord));
+    tipBoard.html(tip);
   };
 
 
